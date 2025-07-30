6984e6b91a2ec7ec0a56d7c4569e9fff0ef73ec1
Parvin Telmanlı <parvin.telmanli@gmail.com>
2025-07-30 12:58:15 +0300

    feat: Add app constants

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,13 @@
+// App-wide constants
+class AppConstants {
+  // Prevent instantiation
+  AppConstants._();
+
+  // App info
+  static const String appName = 'INSIDEX';
+  static const String appTagline = 'Sound Healing & Subliminal';
+
+  // Design size for responsive
+  static const double designWidth = 375;
+  static const double designHeight = 812;
+}
