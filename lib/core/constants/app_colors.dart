5797fd8894ce3561ec12812718bdf11e3c57d8d2
Parvin Telmanlı <parvin.telmanli@gmail.com>
2025-07-30 17:22:23 +0300

    commit

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Prevent instantiation
+  AppColors._();
+
+  // Primary Colors - Renaissance inspired
+  static const Color primaryGold = Color(0xFFD4AF37);
+  static const Color primaryGoldLight = Color(0xFFE6D088);
+  static const Color primaryGoldDark = Color(0xFFB8941F);
+
+  // Light Theme Colors
+  static const Color backgroundWhite = Color(0xFFFFFFFD);
+  static const Color backgroundMarble = Color(0xFFFAF7F5);
+  static const Color backgroundBeige = Color(0xFFF5F0E8);
+
+  static const Color textPrimary = Color(0xFF1A1A1A);
+  static const Color textSecondary = Color(0xFF666666);
+  static const Color textLight = Color(0xFF999999);
+
+  // Dark Theme Colors
+  static const Color darkBackground = Color(0xFF0A0A0A);
+  static const Color darkBackgroundElevated = Color(0xFF141414);
+  static const Color darkBackgroundCard = Color(0xFF1E1E1E);
+
+  static const Color darkTextPrimary = Color(0xFFF5F5F5);
+  static const Color darkTextSecondary = Color(0xFFB0B0B0);
+  static const Color darkTextLight = Color(0xFF808080);
+
+  // Divider Colors
+  static const Color dividerLight = Color(0xFFE0E0E0);
+  static const Color dividerDark = Color(0xFFCCCCCC);
+  static const Color darkDivider = Color(0xFF2A2A2A);
+}
